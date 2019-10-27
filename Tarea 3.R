5b34a269f228acd10a518420a52ecb570f6c9be3
Paula Quinteros <paula.senisman@gmail.com>
2019-10-27 18:00:16 -0300

    Avance

---
+++ Tarea 3.R
@@ -0,0 +1,15 @@
+
+# Se define la lista ------------------------------------------------------
+listaDeNumeros <- list(2,5,6,2,1,5,6,10,11,20,15)
+#Debe contener tipo de dato Númerico
+
+# Acceder de forma directa a través de la posición ------------------------
+listaDeNumeros[5]
+#Esto indica que en la posición 5 está el 1.
+
+
+# Ejercicio 1 -------------------------------------------------------------
+#¿Qué pasa si se reemplaza el 5 por un 0?
+
+listaDeNumeros[0]
+
